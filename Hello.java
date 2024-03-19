853d2f79e418f1a17d32ffb5d153444d806a6b85
Bruce Li <isanoob1897@outlook.com>
2024-03-18 19:45:30 -0700

    add

--- Hello.java
+++ Hello.java
@@ -1,6 +1,14 @@
 // Hello world
+
+// add ui components
+import javax.swing.*;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JFrame frame = new JFrame("Hello");
+        frame.setSize(300, 150);
+        JLabel label = new JLabel("Hello!", JLabel.CENTER);
+        frame.add(label);
+        frame.setVisible(true);
     } 
 }
