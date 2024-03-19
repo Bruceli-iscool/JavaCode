828fdd190c11a6cb855779d7d9f0ac13f81e8226
Bruce Li <isanoob1897@outlook.com>
2024-03-18 20:35:55 -0700

    add

--- Hello.java
+++ Hello.java
@@ -2,13 +2,20 @@
 
 // add ui components
 import javax.swing.*;
+import java.awt.*;
 
 public class Hello {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello");
         frame.setSize(300, 150);
-        JLabel label = new JLabel("Hello!", JLabel.CENTER);
-        frame.add(label);
+        // JLabel label = new JLabel("Hello!", JLabel.CENTER);
+        // frame.add(label);
+        frame.add(new HelloComponent());
         frame.setVisible(true);
     } 
+}
+class HelloComponent extends JComponent {
+    public void paintComponent(Graphics g) {
+     g.drawString("Hello!", 125, 95);
+}
 }
