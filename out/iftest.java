8df994d080c7ca02092fa04ddf110796dac3bd55
Bruce Li <isanoob1897@outlook.com>
2024-03-20 20:33:47 -0700

    add

---
+++ out/iftest.java
@@ -0,0 +1,19 @@
+package out;
+public class iftest {
+    public static void main(String[] args) {
+        // declare variable
+        int i = 5;
+        if (i == 5) {
+            System.out.println("The Number is 5");
+        } else {
+            System.out.println("The Number is not 5");
+        }
+        // simple if statements
+        /* char hi = 'h';
+        if (hi == 'h') 
+            System.out.println("The char is h");
+        else
+            System.out.println("The char is not h");
+        */
+    }
+}
