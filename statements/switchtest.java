2d8504ebfab2f422eb5dbb3d2edb74b47046951f
Bruce Li <isanoob1897@outlook.com>
2024-03-22 19:20:35 -0700

    add

---
+++ statements/switchtest.java
@@ -0,0 +1,16 @@
+package statements;
+
+public class switchtest {
+    public static void main(String[] args) {
+        int hi = 3;
+        switch (hi) {
+            case 4:
+                System.out.println(5);
+                break;
+        
+            default:
+            System.out.println(2);
+                break;
+        }
+    }
+}
