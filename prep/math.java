e318f576392b80411f899cfe1d10c677a8810b10
Bruce Li <isanoob1897@outlook.com>
2024-05-15 17:13:41 -0700

    add

---
+++ prep/math.java
@@ -0,0 +1,16 @@
+package prep;
+
+
+public class math {
+    public static void main(String[] args) {
+        // use math libary
+        Integer c = new Integer(Math.max(0, 10));
+        System.out.println(c);
+        Double a = new Double(Math.sqrt(200));
+        System.out.println(a);
+        c = Math.min(2, 5);
+        System.out.println(c);
+        c = Math.abs(-123);
+        System.out.println(c);
+    }
+}
---
+++ prep/math_random.java
@@ -0,0 +1,11 @@
+package prep;
+import java.util.Random;
+
+
+public class math_random {
+    public static void main(String[] args) {
+        // use java.util.Random (Working with Random and Math class)
+        Random gen = new Random();
+        System.out.println(gen.nextInt(20));
+    }
+}
