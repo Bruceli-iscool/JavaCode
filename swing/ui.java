4b411f4e8fb2a08352a9e31ddde26a47c2f106a4
Bruce Li <isanoob1897@outlook.com>
2024-05-19 17:09:31 -0700

    add

---
+++ swing/ui.java
@@ -0,0 +1,13 @@
+package swing;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class ui {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ui");
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 400);    
+}
+}
