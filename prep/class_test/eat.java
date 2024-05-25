154a621a50c7878acf918a465cad9392cb83864e
Bruce Li <isanoob1897@outlook.com>
2024-05-25 10:51:34 -0700

    add

--- prep/class_test/eat.java
+++ prep/class_test/eat.java
@@ -1,6 +1,10 @@
 package prep.class_test;
-
 public class eat {
-    Animal cat = new Animal(100);
-    
+    public static void main(String args[]) {
+    Animal dogAnimal = new Animal(100);
+    dogAnimal.eat();
+    for (int i = 0;i<100;i++) {
+        dogAnimal.exercise();
+    }
+}
 }
