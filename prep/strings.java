859d42fe58d74b889bfbe3e5c46e96a283c4fc53
Bruce Li <isanoob1897@outlook.com>
2024-05-27 11:11:33 -0700

    add

--- prep/strings.java
+++ prep/strings.java
@@ -1,5 +1,9 @@
 package prep;
 
 public class strings {
-    
+    public static void main(String[] args) {
+        // use string methods
+        String name = "John";
+        System.out.println(name.length());
+    }
 }
