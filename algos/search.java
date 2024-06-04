413320d3d4943cfe6aa93b6b890984ea73a5dd3a
Bruce Li <isanoob1897@outlook.com>
2024-06-04 18:58:58 +0000

    add

---
+++ algos/search.java
@@ -0,0 +1,27 @@
+package algos;
+
+// brute force search
+public class search {
+    public static void main(String[] args) {
+        int[] n = {2, 4, 5};
+        Search s = new Search(n, 4);
+        System.out.println(s.search());
+
+    }
+}
+
+class Search {
+    int[] nums;
+    int target;
+    public Search(int[] arr, int tar) {
+        nums = arr;
+        target = tar;
+    } public boolean search() {
+        for (int y:nums) {
+            if (y == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
