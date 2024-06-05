2664c5d179743f67ec843a773f9b02cc03caa5d3
Bruce Li <isanoob1897@outlook.com>
2024-06-05 16:44:33 +0000

    add

--- algos/binarySearch.java
+++ algos/binarySearch.java
@@ -1,7 +1,35 @@
 package algos;
 
+import java.util.Arrays;
+
 public class binarySearch {
     public static void main(String[] args) {
         // binary search
+        int[] arr = {1, 2, 3, 4, 5, 2};
+        Array k = new Array(arr);
+        
+    }
+}
+
+class Array{
+    int[] arr;
+    public Array(int[] values) {
+        arr = values;
+    } public int binarySearch(int target) {
+        Arrays.sort(arr);
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            double mid = (double)(low + high) / 2;
+            int guess = arr[(int) mid];
+            if (guess == target) {
+                return (int) mid;
+            } else if (guess > target) {
+                high = (int) mid - 1;
+            } else {
+                low = (int) mid + 1;
+            }
+        }
+        return -1;
     }
 }
