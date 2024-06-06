5909772b9c0cdfcb58f7bdf6f64ff86ad8ef0aae
Bruce Li <isanoob1897@outlook.com>
2024-06-06 19:33:33 +0000

    add

--- algos/selectionSort.java
+++ algos/selectionSort.java
@@ -3,5 +3,19 @@ package algos;
 public class selectionSort {
     public static void main(String args[]) {
         // use selection sort
-    } public int[] sort(int[] i)
+        // time = O(n^2)
+    } public int[] sort(int[] i) {
+        return int[];
+    } private int smallest(int[] i) {
+        // find smallest
+        int smallest = i[0];
+        int smallestIndex = 0;
+        for (int y = 0;y<i.length;y++) {
+            if (i[y] < smallest) {
+                smallest = i[y];
+                smallestIndex = y;
+            }
+        }
+        return smallestIndex;
+    }
 }
