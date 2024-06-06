7ed573e823beb614a673a8f1e8aec1f9bfe81033
Bruce Li <isanoob1897@outlook.com>
2024-06-05 21:16:00 -0700

    add

--- algos/binarySearch.java
+++ algos/binarySearch.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class binarySearch {
     public static void main(String[] args) {
-        // binary search
+        // binary search 
+        // time: O(log n)
         int[] arr = {1, 2, 3, 4, 5, 2};
         Array k = new Array(arr);
         System.out.println(k.binarySearch(6));
