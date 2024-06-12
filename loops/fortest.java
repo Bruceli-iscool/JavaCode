3321298226731e7cad82e437e07323cdf98dad83
Bruce Li <isanoob1897@outlook.com>
2024-06-12 11:22:14 -0700

    add

--- loops/fortest.java
+++ loops/fortest.java
@@ -2,8 +2,14 @@ package loops;
 
 public class fortest {
     public static void main(String[] args) {
+        // counter style for loops
         for (int i = 0;i <10;i++) {
             System.out.println(i);
         }
+        // enhanced for loop
+        // an array
+        int[] arr = {2, 3, 5, 7};
+        // print every value in the array
+        
     }
 }
