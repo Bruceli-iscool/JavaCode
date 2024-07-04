d57346cd310422d8757b067f180fe02be8040726
Bruce Li <isanoob1897@outlook.com>
2024-07-04 10:18:33 -0700

    add

--- prep/enchancedFor.java
+++ prep/enchancedFor.java
@@ -4,7 +4,7 @@ public class enchancedFor {
     public static void main(String[] args) {
         // enchanced for loop demo
         // loop through every item in array
-        String[] names = {"John", "Bill", "Max", "Bodie"};
+        String[] names = {"John", "Bill", "Max", "Bodie", "Steve"};
         for (String a:names) {
             System.out.println("His name is " + a);
         }
