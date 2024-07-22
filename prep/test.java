9c8590c4b6ff9b22063498c4e64f70d1be2c98dc
Bruce Li <isanoob1897@outlook.com>
2024-07-22 09:54:06 -0700

    add

---
+++ prep/test.java
@@ -0,0 +1,17 @@
+public class test {
+public static void main(String[] args) {
+
+}
+}
+
+// inheritence
+class Car {
+public int age;
+public String model;
+public Car(int a, int m) {
+age = a;
+model = m;
+}
+}
+class EV extends Car {
+}
