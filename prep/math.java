f47e359b7d912fc7b38d41e7bf4f2c43b9c44c77
Bruce Li <isanoob1897@outlook.com>
2024-07-24 09:12:06 -0700

    add

--- prep/math.java
+++ prep/math.java
@@ -8,7 +8,7 @@ public class math {
         // testing decapritated declarations
         Integer c = new Integer(Math.max(0, 10));
         System.out.println(c);
-        Double a = new Double(Math.sqrt(200));
+        Double a = new Double(Math.sqrt(200)); 
         System.out.println(a);
         c = Math.min(2, 5);
         System.out.println(c);
