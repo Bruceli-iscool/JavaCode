b348a44901e10b437667bb52836bba08cd452c11
Bruce Li <isanoob1897@outlook.com>
2024-08-01 11:48:04 -0700

    add

--- loops/fortest.java
+++ loops/fortest.java
@@ -8,7 +8,7 @@ public class fortest {
         }
         // enhanced for loop
         // an array
-        int[] arr = {2, 3, 5, 7};
+        int[] arr = {2, 3, 5, 7}; 
         // print every value in the array 
         for (int i:arr) { 
             System.out.println(i); 
