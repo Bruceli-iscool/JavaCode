2b94c27993406dd477717c94d414f1cc58625318
Bruce Li <isanoob1897@outlook.com>
2024-08-13 16:36:23 -0700

    add

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class Hello {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Hello");
+        JFrame frame = new JFrame("Hello"); 
         frame.setSize(300, 150);
         // JLabel label = new JLabel("Hello!", JLabel.CENTER);
         // frame.add(label);
