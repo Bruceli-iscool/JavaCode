368def2498bd8b8a71260b519d1df3ff7f280263
Bruce Li <isanoob1897@outlook.com>
2024-09-09 14:08:36 -0700

    add

--- swing/ui.java
+++ swing/ui.java
@@ -7,7 +7,7 @@ public class ui {
     public static void main(String[] args) {
         JFrame frame = new JFrame("ui");
         frame.setLayout(new FlowLayout());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setSize(300, 400);
         JLabel label = new JLabel("Hi john\n");
         frame.add(label);
