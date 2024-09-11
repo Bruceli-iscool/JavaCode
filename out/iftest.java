ecd7729acddcbe32a2c76e6c478dcf99c8b441e0
Bruce Li <isanoob1897@outlook.com>
2024-09-11 11:57:13 -0700

    add

--- out/iftest.java
+++ out/iftest.java
@@ -10,7 +10,7 @@ public class iftest {
         }
         // simple if statements
         /* char hi = 'h'; 
-        if (hi == 'h') 
+        if (hi == 'h')  
             System.out.println("The char is h"); 
         else
             System.out.println("The char is not h");
