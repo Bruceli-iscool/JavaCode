6306bac2bd7f027a14ed75a2e7be517efb0e5673
Bruce Li <isanoob1897@outlook.com>
2024-09-18 07:28:00 -0700

    add

--- guess/guessUI.java
+++ guess/guessUI.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class guessUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame("guess");  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         frame.setVisible(true); 
         
     };  
