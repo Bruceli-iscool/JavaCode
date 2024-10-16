15bbdbf9c7d601a4df6563ab254e0ba9b8656e19
Bruce Li <isanoob1897@outlook.com>
2024-10-16 07:34:18 -0700

    add

--- effective/test.java
+++ effective/test.java
@@ -1,7 +1,7 @@
 package effective;
 
 public class test {
-    public static void main(String args[]) {
+    public static void main(String args[]) {  
          Bank bank = new Bank();    
     };
 };  
