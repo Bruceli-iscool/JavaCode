81384d3c091c5a9f507dd2763378223543d34f38
Bruce Li <isanoob1897@outlook.com>
2024-10-31 07:47:55 -0700

    add

--- loops/whileTest.java
+++ loops/whileTest.java
@@ -2,7 +2,7 @@ package loops;
 
 public class whileTest {  
   public static void main (String args[]) {
-    int i = 10;
+    int i = 10; 
     while (i < 40) {  
       System.out.println(i);
       i += 1;
