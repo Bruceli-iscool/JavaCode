7d7db3d9c79c1c358920d2d6d0d8dcb5bdd72dae
Bruce Li <isanoob1897@outlook.com>
2024-11-07 07:33:12 -0800

    add

--- effective/test.java
+++ effective/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String args[]) {  
          Bank bank = new Bank();    
     };    
-};  
+};       
 // testing enums       
 enum weekDays {  
     MONDAY, 
